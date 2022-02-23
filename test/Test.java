cc71f4f5a75e0f31899fdc43aafda4b1b72f5390
fandacheng <1094228044@qq.com>
2022-02-23 22:22:37 +0800

    main第4次提交

--- test/Test.java
+++ test/Test.java
@@ -1,2 +1,14 @@
 public class Test {
+    public static void main(String[] args) {
+        for (int i = 0; ; i++){
+            double j = Math.random()*10000;
+            //System.out.println(j);
+            if (j < 1){
+                System.out.println(i);
+                System.out.println(j);
+
+            }
+
+        }
+    }
 }
--- test/Test3.java
+++ test/Test3.java
@@ -1,4 +1,17 @@
 public class Test3 {
     private String aa;
     private String a3;
+
+    public static void main(String[] args) {
+        new Test3().a1();
+
+        System.out.println("你好");
+
+        {
+            System.out.println("hello");
+        }
+    }
+    private void a1(){
+        System.out.println("测试");
+    }
 }
